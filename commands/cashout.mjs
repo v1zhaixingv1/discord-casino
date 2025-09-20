381a6d517388b4c71605f9575dc49121c3db93ed
FatterCatDev <kevin_y_jones@hotmail.com>
2025-09-20 05:39:45 +0000

    Auto update: 2025-09-20T05:39:45+00:00

--- commands/cashout.mjs
+++ commands/cashout.mjs
@@ -15,11 +15,11 @@ export default async function handleCashOut(interaction, ctx) {
     await ctx.postCashLog(interaction, [
       `💸 **Cash Out**`,
       `User: <@${target.id}> • Amount: **${ctx.chipsAmount(amount)}**${reason ? ` • Reason: ${reason}` : ''}`,
-      // `User: Thank you Kitten! <@${target.id}> • Amount: **${ctx.chipsAmount(amount)}**${reason ? ` • Reason: ${reason}` : ''}`,
+      // `User: My indulgent Kitten <@${target.id}> • Amount: **${ctx.chipsAmount(amount)}**${reason ? ` • Reason: ${reason}` : ''}`,
       `User Chips (after): **${ctx.chipsAmount(chips)}**`
     ]);
     return interaction.reply({ content: `✅ Burned **${ctx.chipsAmount(amount)}** from <@${target.id}>${reason ? ` (${reason})` : ''}.`, ephemeral: true });
-    // return interaction.reply({ content: `✅ Burned **${ctx.chipsAmount(amount)}** from Kitten! <@${target.id}>${reason ? ` (${reason})` : ''}.`, ephemeral: true });
+    // return interaction.reply({ content: `✅ Burned **${ctx.chipsAmount(amount)}** from my temptress Kitten <@${target.id}>${reason ? ` (${reason})` : ''}.`, ephemeral: true });
   } catch (e) {
     console.error(e);
     return interaction.reply({ content: '❌ Something went wrong.', ephemeral: true });
